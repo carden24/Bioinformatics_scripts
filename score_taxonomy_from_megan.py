395c3cef46b30eaa0822110261dcbd8e193ba114
carden24 <carden24@mail.ubc.ca>
2014-09-15 13:00:36 -0700

    I lost my hopes for lab meeting

--- score_taxonomy_from_megan.py
+++ score_taxonomy_from_megan.py
@@ -8,12 +8,21 @@ filein = open(sys.argv[1], 'rb')
 fileout = open(sys.argv[1] + '.score', 'w')
 
 for line in filein:
-     line0 = line.split(';')
-     phyla = line0[8]
-     phyla = phyla.lstrip()
-     phyla_score = score_dictionary.get(phyla, 0)
-     phyla_score = phyla_score + 1
-     score_dictionary[phyla] = phyla_score
+    line0 = line.split(';')
+#     test_assign = line0[2]
+#     print line0
+#     if test_assign == ' Not assigned':
+    if len(line0) <= 7:
+        phyla = 'NA'
+        phyla_score = score_dictionary.get(phyla, 0)
+        phyla_score = phyla_score + 1
+        score_dictionary[phyla] = phyla_score
+    else:
+        phyla = line0[8]
+        phyla = phyla.lstrip()
+        phyla_score = score_dictionary.get(phyla, 0)
+        phyla_score = phyla_score + 1
+        score_dictionary[phyla] = phyla_score
 
 #write score_dictionary
 for key1, value1 in score_dictionary.iteritems():
