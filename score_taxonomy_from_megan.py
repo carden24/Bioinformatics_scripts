2422c716cf44c97f9e0a84ccd0f79e4116730f89
carden24 <carden24@mail.ubc.ca>
2014-09-15 12:33:08 -0700

    adding scoring of megan

---
+++ score_taxonomy_from_megan.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# score_taxonomy_from_megan.py
+python 
+
+score_dictionary = {}
+filein = open(sys.argv[1], 'rb')
+fileout = open(sys.argv[1] + '.core', 'rb')
+
+for line in open (filein, 'rb'):
+     line0 = line.split(';')
+     phyla = line[8]
+     phyla = phyla.lstrip()
+     phyla_score = score_dictionary.get(phyla, 0)
+     phyla_score = phyla_score + 1
+     score_dictionary[phyla] = phyla_score
+
+#write score_dictionary
+for key1, value1 in score_dictionary.iteritems():
+   fileout.write("%s\t%d\n" %(key1, value1))
+
+filein.close()
+fileout.close()
+
+     
