dff1962608e559468496f8df508504651714da59
carden24 <carden24@mail.ubc.ca>
2014-11-25 16:32:49 -0800

    adding a fast fasta stats scripts

---
+++ stats_fasta_fast.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#usage
+#python fasta.stats.v2.py <filein>
+
+import sys
+import Bio
+from Bio import SeqIO
+
+filein = open(sys.argv[1], 'r')
+
+counter = 0
+total_len = 0
+
+for seq_record in SeqIO.parse(filein, format = "fasta"):
+   len_seq = len(seq_record.seq)
+   total_len = total_len + len_seq
+   counter = counter + 1
+
+print '%s\t%s\t%s' %(sys.argv[1], str(counter), str(total_len))
+
+
+
--- stats_fasta_v2.py
+++ stats_fasta_v2.py
@@ -8,26 +8,26 @@ import Bio
 from Bio import SeqIO
 import numpy
 
-filein=open(sys.argv[1],'r')
+filein = open(sys.argv[1], 'r')
 
-counter=0
-total_len=[]
+counter = 0
+total_len = []
 
-for seq_record in SeqIO.parse(filein, format="fasta"):
-   len_seq=len(seq_record.seq)
+for seq_record in SeqIO.parse(filein, format = "fasta"):
+   len_seq = len(seq_record.seq)
    total_len.append(len_seq)
-   counter=counter+1
+   counter = counter + 1
 
 #histo=numpy.histogram(total_len)
 #print histo
 
-len_sum=numpy.sum(total_len)
+len_sum = numpy.sum(total_len)
 #print len_sum
 
-average_len=len_sum/float(counter)
+average_len = len_sum/float(counter)
 #print average_len
 
-median_len=numpy.median(total_len)
+median_len = numpy.median(total_len)
 
 print 'Reads found: %s' %counter
 print 'Total bases: %s' % len_sum
